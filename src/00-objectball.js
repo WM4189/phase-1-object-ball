e1c8b96af56b9f89e66572fcd5a9045e48aeadee
William Mosto <will.mosto@gmail.com>
2021-08-13 11:32:41 -0400

    Declared Object

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,6 +1,6 @@
 function gameObject(){
-    {
-        "home" :{
+const objBall =  {
+        "home" : {
             "teamName": "Brooklyn Nets" ,
             "colors" : ["Black", "White"] ,
             "players" : {
@@ -10,8 +10,8 @@ function gameObject(){
                         "Mason Plumlee" : {"number":1,"shoe":19,"points":26,"rebounds":12,"assists":6,"steals":3,"blocks":8,"slamDunks":5},
                         "Jason Terry" : {"number":31,"shoe":51,"points":19,"rebounds":2,"assists":2,"steals":4,"blocks":11,"slamDunks":1} 
                         } 
-        }, 
-        "away" :{
+                }, 
+        "away" : {
                 "teamName": "Charlotte Hornets" ,
                 "colors" : ["Turquoise", "Purple"],
                 "players" : {
@@ -21,9 +21,8 @@ function gameObject(){
                     "Ben Gordon" : {"number":8,"shoe":15,"points":33,"rebounds":3,"assists":2,"steals":1,"blocks":1,"slamDunks":0},
                     "Brendan Haywood" : {"number":33,"shoe":15,"points":6,"rebounds":12,"assists":12,"steals":22,"blocks":5,"slamDunks":12} 
                     } 
-                },  
-    },
-
-
-
+                }  
+                }
 }
+
+console.log(gameObject());
